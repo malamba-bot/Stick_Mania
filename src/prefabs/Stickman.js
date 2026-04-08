62ffcc86477fa3a0ae436fe0a00f9d12b787ab89
Max Kinet <mkinet@ucsc.edu>
2026-04-08 13:13:07 -0700

    add physics bodies to construct player hitbox

--- src/prefabs/Stickman.js
+++ src/prefabs/Stickman.js
@@ -33,12 +33,21 @@ export class Stickman extends Phaser.GameObjects.Container {
 
 
         // Add physics bodies
-        let torso = this.scene.physics.add.image(-50, 0, null);
-        torso.setVisible(false);
-        torso.setSize(20, 400);
+        this.create_physics_body(-50, -3, 10, 65, false); // torso
+        this.create_physics_body(-68, -100, 80, 0, true); // head
+        this.create_physics_body(-50, 43, 50, 0, true); // legs
+    }
 
-        this.add(torso);
-        this.add(sprite);
+    create_physics_body(x, y, w, h, circle) {
+        let body = this.scene.physics.add.image(x, y, null);
+        body.setVisible(false);
+
+        if (circle) {
+            body.setCircle(w / 2);
+        } else
+            body.setSize(w, h);
+        
+        this.add(body);
     }
 
     init_animations() {
