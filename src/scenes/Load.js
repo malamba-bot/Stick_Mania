e019f46e0287c0109f251e3fb24381ed8e13a7c5
Max Kinet <mkinet@ucsc.edu>
2026-04-08 12:32:47 -0700

    fix file structure and move preloading to its own scene

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,11 +4,18 @@ export class Load extends Phaser.Scene {
     }
 
     preload() {
-        this.load.setPath('../assets/stickman');
+        this.load.setPath('../../assets/');
+
+        this.load.image('background', 'Background.png');
+
+        // Load stickman assets here
+        this.load.setPath('../../assets/stickman/');
+
+        this.load.aseprite('player', 'StickManIdle.png', 'StickManIdle.json');
         this.load.image('base_stance', 'base_stance.png');
     }
 
     create() {
-
+        this.scene.start('MainMenu')
     }
 }
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,9 +7,6 @@ export class MainMenu extends Phaser.Scene {
     }
 
     preload() {
-        // load assets here
-        this.load.image('background', 'src/assets/Background.png');
-        this.load.aseprite('player', 'src/assets/StickmanIdle.png', 'src/assets/StickmanIdle.json');
     }
 
 
