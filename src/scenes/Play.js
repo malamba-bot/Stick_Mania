70e9bb938c8c926fd1db8a9e8d3cb426ee38dc1a
Max Kinet <mkinet@ucsc.edu>
2026-04-14 23:32:53 -0700

    add iframes to prevent constant damage

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -181,11 +181,10 @@ export class Play extends Phaser.Scene {
 
                 if(!objA || !objB) continue;
 
-                //player punch hits enemy
                 if(objA instanceof Stickman && objB instanceof Stickman) {
-                    if (objA.attacking) 
+                    if (objA.attacking && !objB.invincible) 
                         objB.takeDamage(10);
-                    if (objB.attacking) 
+                    if (objB.attacking && !objA.invincible) 
                         objA.takeDamage(10);
                     //console.log('This hoe hit an enemy!', objA.health.value);
                 }
